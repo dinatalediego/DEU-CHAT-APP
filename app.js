7fb12d855529655b2c1841e565f9168b198779d5
Diego Di Natale <49383324+dinatalediego@users.noreply.github.com>
2024-09-30 01:43:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ document.getElementById('translateButton').addEventListener('click', async () =>
     }
 
     try {
-        const response = await fetch('https://deu-chat-be-app.vercel.app/translate', {
+        const response = await fetch('https://flask-app-ten-omega.vercel.app/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
